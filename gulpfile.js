cd2a41d1282449a67cf5b26efb5850ed4f55fd67
essdot <jsippel@fool.com>
2014-02-12 19:40:59 -0800

    Clean up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,24 +6,30 @@ var clean = require('gulp-clean');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 
+var paths = {
+	outputDir: 'min',
+	specs: [ 'test/iexpect-spec.js' ],
+	iexpectModule: 'app/iexpect.js',
+};
+
 gulp.task('clean', function() {
-	gulp.src('./min', { read: false })
+	gulp.src(paths.outputDir, { read: false })
 		.pipe(clean());
 });
 
 gulp.task('prepare-spec', function() {
-	gulp.src('test/iexpect-spec.js')
+	gulp.src(paths.specs)
 		.pipe(browserify())
 		.pipe(rename('iexpect-spec.browserified.js'))
-		.pipe(gulp.dest('./min'));
+		.pipe(gulp.dest(paths.outputDir));
 });
 
 gulp.task('prepare-iexpect-standalone', function() {
-	gulp.src('app/iexpect.js')
+	gulp.src(paths.iexpectModule)
 		.pipe(browserify({ standalone: 'iexpect' }))
 		.pipe(uglify())
 		.pipe(rename('iexpect.min.js'))
-		.pipe(gulp.dest('./min'));
+		.pipe(gulp.dest(paths.outputDir));
 });
 
 gulp.task('build', [ 'clean', 'prepare-iexpect-standalone', 'prepare-spec' ]);
